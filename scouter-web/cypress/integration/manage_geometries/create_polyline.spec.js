9340334ec4ba70512c400f1fea5a5911c32566b6
fpezzati <fpezzati@gmail.com>
2022-04-14 23:58:58 +0200

    Almost having my first BDD testgit add -A!

---
+++ scouter-web/cypress/integration/manage_geometries/create_polyline.spec.js
@@ -0,0 +1,22 @@
+describe('ScouterWeb map is ready', ()=>{
+  it('map is ready', ()=>{
+    cy.visit('http://192.168.49.2:30000/scouter/');
+    cy.get('scouter-web').shadow().find('.scouter-bound').contains('map');
+  })
+});
+
+describe('Draw geometries on map', ()=>{
+  beforeEach(() => {
+    cy.visit('http://192.168.49.2:30000/scouter/')
+  })
+
+  it('cannot draw on map until draw button is clicked', ()=>{
+    cy.get('scouter-web').shadow().find('.scouter-bound').find('#map').click(300, 300).click(300,400)
+  })
+
+  it('can draw after clicking draw button', ()=>{
+    cy.get('scouter-web').shadow().find('.scouter-tools-main-button').click().should(elem => {
+      elem.parent().find('draw route').click()
+    });
+  })
+});
