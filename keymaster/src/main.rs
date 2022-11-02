b06b90ad98899a2741ddf531e5904471835d3883
fpezzati <fpezzati@gmail.com>
2022-11-02 23:21:22 +0100

    got something that compiles.

--- keymaster/src/main.rs
+++ keymaster/src/main.rs
@@ -1,11 +1,22 @@
 mod server;
 
+use std::fs::File;
+use std::io::Read;
+
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
     let srv = server::server::Server {
         host: "0.0.0.0".to_string(),
-        port: "3000".to_string()
+        port: "3000".to_string(),
+        private_key: load_key("private.pem").unwrap(),
+        public_key: load_key("public.pem").unwrap()
     };
     srv.start().await;
+}
+
+fn load_key(filename: &str) -> Result<String, Box<dyn std::error::Error>> {
+  let mut key = String::new();
+  File::open(filename)?.read_to_string(&mut key)?;
+  Ok(key)
 }
--- keymaster/src/server/mod.rs
+++ keymaster/src/server/mod.rs
@@ -1,38 +1,46 @@
 pub mod server {
 
     use axum::{
-        routing::get,
-        routing::post,
-        Router,
-        Json,
-        http::StatusCode,
-        response::IntoResponse
+      extract::Extension,
+      routing::get,
+      routing::post,
+      Router,
+      Json,
+      http::StatusCode,
+      response::IntoResponse
     };
     use serde_json::json;
     use serde::{ Deserialize, Serialize };
     use std::net::SocketAddr;
     use uuid::Uuid;
+    use jwt_simple::prelude::*;
+    use std::fs::File;
+    use std::io::Read;
+    use std::sync::Arc;
 
     pub struct Server {
         pub host: String,
-        pub port: String
+        pub port: String,
+        pub public_key: String,
+        pub private_key: String
     }
 
     impl Server {
         pub async fn start(&self) {
-            let routes = Router::new()
-              .route("/", get(hello))
-              .route("/check", post(check))
-              .route("/auth", post(auth))
-              .route("/auth2", post(auth2));
-            let mut hostport = String::from(&self.host);
-            hostport.push_str(":");
-            hostport.push_str(&self.port);
-            println!("{}", hostport);
-            let addr : SocketAddr = hostport.parse().expect("invalid host:port pair");
-            axum::Server::bind(
-                &addr
-            ).serve(routes.into_make_service()).await.unwrap();
+          let public_key = String::from(&self.public_key.clone());
+          let routes = Router::new()
+            .route("/", get(hello))
+            .route("/check", post(check))
+            .route("/auth", post(auth))
+            .route("/auth2", post(auth2)).layer(Extension(public_key));
+          let mut hostport = String::from(&self.host);
+          hostport.push_str(":");
+          hostport.push_str(&self.port);
+          println!("{}", hostport);
+          let addr : SocketAddr = hostport.parse().expect("invalid host:port pair");
+          axum::Server::bind(
+              &addr
+          ).serve(routes.into_make_service()).await.unwrap();
         }
     }
 
@@ -54,9 +62,13 @@ pub mod server {
       })))
     }
 
-    async fn auth2(Json(payload): Json<LoginInput>) -> impl IntoResponse {
+    async fn auth2(server_config: Extension<&str>, Json(payload): Json<LoginInput>) -> impl IntoResponse {
+//      let claims = Claims::create(Duration::from_hours(1));
+//      RS384PublicKey::from_pem("something").sign(claims)?;
+//      println!("wtf {}", server_config);
       let lo = LoginOutput{
-        token: payload.username
+        token: payload.username,
+        something: "WTF".to_string()
       };
       (StatusCode::OK, Json(lo))
     }
@@ -70,7 +82,8 @@ pub mod server {
     #[derive(Serialize)]
     struct LoginOutput {
 //      type: "Bearer".to_string();
-      token: String
+      token: String,
+      something: String
     }
 
     struct Error {
